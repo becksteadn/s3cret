bf5247f5104ec9f040b8d1540eb752cd565fcd10
Nathaniel Beckstead <becksteadn@gmail.com>
2019-08-04 18:50:19 -0400

    basic infra

--- function/main.py
+++ function/main.py
@@ -1,4 +1,6 @@
 import boto3
+import botocore
 
 def handler(event, context):
-    return {'hello', 'world'}
+    s3 = boto3.resource('s3')
+    return {'success': 'true'}
--- infra.tf
+++ infra.tf
@@ -3,25 +3,78 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "s3cret_function" {
-
+    name = "s3cret_function"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
 }
 
-resource "aws_iam_policy" "s3cret_function" {
-
+resource "aws_iam_policy" "S3Upload" {
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:TODO",
+            "Resource": "${aws_s3_bucket.s3cret_bucket.arn}"
+        }
+    ]
+}
+EOF
 }
 
-resource "aws_iam_policy_attachment" "s3cret_attachment" {
-
+resource "aws_iam_policy" "S3Download" {
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:TODO",
+            "Resource": "${aws_s3_bucket.s3cret_bucket.arn}"
+        }
+    ]
 }
+EOF
+}
+
+resource "aws_iam_policy_attachment" "S3Upload" {
+    name = "s3cret_upload"
+    roles = ["${aws_iam_role.s3cret_function.name}"]
+    policy_arn = "${aws_iam_policy.S3Upload.arn}"
+}
+
+resource "aws_iam_policy_attachment" "S3Download" {
+    name = "s3cret_download"
+    roles = ["${aws_iam_role.s3cret_function.name}"]
+    policy_arn = "${aws_iam_policy.S3Download.arn}"
+}
+
 
 resource "aws_s3_bucket" "s3cret_bucket" {
-
+    bucket = "s3cret"
+    acl = "private"
 }
 
-resource "aws_lambda_function" "s3cret_function" {
-
-}
-
-resource "aws_api_gateway_rest_api" "s3cret_api" {
-    
+resource "aws_lambda_function" "s3cret" {
+    function_name = "s3cret"
+    filename = "function/build/build.zip"
+    source_code_hash = "${filebase64sha256("function/build/build.zip")}"
+    role = "${aws_iam_role.s3cret_function.arn}"
+    handler = "main.handler"
+    runtime = "python3.7"
+    timeout = "5"
 }
