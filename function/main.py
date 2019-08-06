f41554ffb78037a58c4f51d867f007b0c70d2426
Nathaniel Beckstead <becksteadn@gmail.com>
2019-08-05 20:58:30 -0400

    start crypto

--- function/main.py
+++ function/main.py
@@ -1,6 +1,24 @@
 import boto3
 import botocore
+import binascii
+import base64
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+def gen_key(pass):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(pass)
+    return base64.urlsafe_b64encode(digest.finalize())
 
 def handler(event, context):
     s3 = boto3.resource('s3')
+
+    key = gen_key(event['password'])
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt('hello world')
+
+    plain_text = cipher_suite.decrypt(cipher_text)
+
     return {'success': 'true'}
